d45e4d997ee5071b81dad3e753ab07f039a10a73
Amina <48387428+minniedd@users.noreply.github.com>
2024-07-26 16:50:13 +0200

    Create DoctorsGetAllEndpoint.cs

---
+++ VitaCare_API/VitaCare_API/Endpoints/MedicalWorker/GetAllDoctors/DoctorsGetAllEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sql;
+using Microsoft.EntityFrameworkCore;
+using VitaCare_API.Data;
+using VitaCare_API.Helpers;
+
+namespace VitaCare_API.Endpoints.MedicalWorker.GetAllDoctors
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsGetAllEndpoint : MyBaseEndpoint<NoRequest, DoctorsGetAllResponse>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public DoctorsGetAllEndpoint(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public override async Task<DoctorsGetAllResponse> Obradi([FromQuery]NoRequest request)
+        {
+            var data = await _applicationDbContext.Doctor.Select(x => new DoctorsGetAllResponseDoctors
+            {
+                doctorID = x.DoctorID,
+                firstName = x.FirstName,
+                lastName = x.LastName
+            }).ToListAsync();
+
+
+            return new DoctorsGetAllResponse
+            {
+                Doctors = data
+            };
+        }
+    }
+}
